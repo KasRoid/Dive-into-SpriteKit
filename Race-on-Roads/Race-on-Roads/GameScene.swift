e032c59b272c8cfa3ca56f8924ae562ab4273ee1
KasRoid <KasRoid@gmail.com>
2021-12-23 10:04:31 +0900

    UI: Added Background

---
+++ Race-on-Roads/Race-on-Roads/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  Race-on-Roads
+//
+//  Created by Kas Song on 12/23/21.
+//
+
+import GameplayKit
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "road.jpg")
+        background.zPosition = -1
+        background.size = CGSize(width: frame.width, height: frame.height)
+        addChild(background)
+    }
+}
--- Race-on-Roads/Race-on-Roads/ViewController.swift
+++ Race-on-Roads/Race-on-Roads/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view = SKView()
+        guard let view = view as? SKView,
+              let scene = SKScene(fileNamed: "GameScene") else { fatalError() }
+        scene.scaleMode = .aspectFill
+        view.presentScene(scene)
+        view.preferredFramesPerSecond = 120
+        view.ignoresSiblingOrder = true
+        view.showsFPS = true
+        view.showsNodeCount = true
     }
-
-
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
 }
-
