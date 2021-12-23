75d1ee8b6cec32c4740c8b6f831971b5ccae4f42
KasRoid <KasRoid@gmail.com>
2021-12-23 09:04:40 +0900

    init: Created Race-on-Roads Project

---
+++ Race-on-Roads/Race-on-Roads/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Race-on-Roads
+//
+//  Created by Kas Song on 12/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
